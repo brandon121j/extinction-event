3263656638eac9d22cf171b70666d060ac159b8d
Brandon <brandon121j@DESKTOP-DPFP3JR.localdomain>
2021-07-19 16:32:59 -0400

    Completion of assignment

---
+++ main.js
@@ -0,0 +1,67 @@
+const li = document.querySelectorAll('ol li');
+
+const fading = document.querySelectorAll('ul li');
+
+const pics = document.querySelectorAll('#row img')
+
+const button = document.querySelector('button')
+
+button.style.cursor = 'pointer';
+
+button.addEventListener('click', meteor);
+
+for (const looper of li) {
+    looper.addEventListener('click', onClick);
+    looper.style.cursor = 'pointer';
+}
+
+for (const looping of fading) {
+    looping.addEventListener('click', fade);
+    looping.style.cursor = 'pointer';
+}
+
+for (const pic of pics) {
+    pic.addEventListener('click', disappear);
+    pic.style.cursor = 'pointer';
+    pic.style.opacity = '1';
+}
+
+function onClick() {
+    if (this.style.textDecoration === 'line-through') {
+        this.style.textDecoration = 'none';
+    } else {
+        this.style.textDecoration = 'line-through'
+    }
+
+}
+
+function fade() {
+    this.style.opacity = '0.0';
+}
+
+function disappear(event) {
+    if (event.target.style.opacity === '1') {
+        event.target.style.opacity = '0.0'
+    } else {
+        event.target.style.opacity = '1'
+    }
+}
+
+function meteor(event) {
+
+    li.forEach(element => {
+        element.style.opacity = '0.0'
+    })
+
+    fading.forEach(element => {
+        element.style.opacity = '0.0'
+    })
+
+    pics.forEach(element => {
+        element.style.opacity = '0.0'
+    })
+
+}
+
+// li.addEventListener('click', onClick)
+
